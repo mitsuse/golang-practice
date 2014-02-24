092007746a693ba21d8a38b2bdb7cba40a705994
Tomoya Kose <tomoya@mitsuse.jp>
2014-02-24 11:03:15 +0900

    A Tour of Go - 43. Exercise: Maps

---
+++ tour/map.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "strings"
+    "code.google.com/p/go-tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+    word_map := make(map[string]int)
+    for _, word := range strings.Fields(s) {
+        _, contained := word_map[word]
+        if contained {
+            word_map[word] += 1
+        } else {
+            word_map[word] = 1
+        }
+    }
+    return word_map
+}
+
+func main() {
+    wc.Test(WordCount)
+}
